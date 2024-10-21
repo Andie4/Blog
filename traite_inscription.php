1be643985204c93fb3329bb1327a3b7a63c20b2b
Andréa.C <155273473+Andie4@users.noreply.github.com>
2024-10-21 11:15:11 +0200

    Update traite_inscription.php
    L'inscription fonctionne cependant elle ne prend pas en compte lorsque le login est déjà utilisé. Il y a un bug lors de la connexion, le mot de passe réel ne fonctionne plus à cause du hash du mot de passe dans la base de données.

--- traite_inscription.php
+++ traite_inscription.php
@@ -5,20 +5,25 @@ $requete="SELECT * FROM users WHERE login=:login";
 $stmt=$db->prepare($requete);
 $stmt->bindParam(':login',$_GET["login"], PDO::PARAM_STR);
 $stmt->execute();
-var_dump
 
-
+// le login est déjà utilisé
     if ($utilisateur=$stmt->fetch(PDO::FETCH_ASSOC)){
         header('Location:saisie_inscription.php?err=login');
 }
+// ajout d'un nouvel utilisateur à la base de données
     else {
-        $requete= "INSERT INTO utilisateur VALUES (:prenom,:login,:mot_de_passe)";
+        $requete= "INSERT INTO utilisateur (prenom, login, mot_de_passe) VALUES (:prenom,:login,:mot_de_passe)";
         $stmt= $db->prepare($requete);
         $stmt->bindValue(':prenom',$_GET["prenom"] , PDO::PARAM_STR);
         $stmt->bindValue(':login',$_GET["login"] , PDO::PARAM_STR); 
         $hash= password_hash($_GET["mot_de_passe"],PASSWORD_DEFAULT);
         $stmt->bindValue(':mot_de_passe',$hash , PDO::PARAM_STR); 
-        $stmt->execute();
+
+}   // Vérification de l'ajout de l'utilisateur à la  base de données
+    if ($stmt->execute()) {
+    echo "Utilisateur ajouté avec succès."; 
+}   else {
+    echo "Erreur lors de l'ajout de l'utilisateur.";
 }
 
 ?>
