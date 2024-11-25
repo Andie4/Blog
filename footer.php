51f3420fd3eb20dcaa58521c50a6b9d48ddcb58b
Andréa.C <155273473+Andie4@users.noreply.github.com>
2024-11-25 23:30:03 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,86 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="designFooter">
+    <footer class=" text-center">
+    <!-- Grid container -->
+    <div class="container">
+      <!-- Section: Links -->
+      <section class="mt-0.5">
+        <!-- Grid row-->
+        <div class="row text-center d-flex justify-content-center pt-5">
+          <!-- Grid column -->
+          <div class="col-md-2">
+            <h6 class="text-uppercase font-weight-bold">
+             <a class="nav-link" aria-current="page" href="index.php">Accueil</a>
+        </h6>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2">
+            <h6 class="text-uppercase font-weight-bold">
+                <a class="nav-link" href="saisie_login.php">Connexion</a>
+            </h6>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2">
+            <h6 class="text-uppercase font-weight-bold">
+                <a class="nav-link" href="saisie_inscription.php">Inscription</a>
+            </h6>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2">
+            <h6 class="text-uppercase font-weight-bold">
+                <a class="nav-link" href="archive.php">Archive</a>
+            </h6>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2">
+            <h6 class="text-uppercase font-weight-bold">
+                <a class="nav-link" aria-disabled="true" href="profil.php">Profil</a>
+            </h6>
+          </div>
+          <!-- Grid column -->
+          
+        </div>
+        <!-- Grid row-->
+      </section>
+      <!-- Section: Links -->
+
+      <hr class="my-5" />
+
+      <!-- Section: Text -->
+      <section class="mb-0.5">
+        <div class="row d-flex justify-content-center">
+          <div class="col-lg-8">
+            <h6 class="text-uppercase font-weight-bold"><a href="mentions.php" class="nav-link">Mentions légales</a></h6>
+          </div>
+        </div>
+      </section>
+    </div>
+  </footer>
+    </section>
+
+
+
+
+
+    <!-- source du footer : http://localhost:8888/Blog/footer.php -->
+</body>
+</html>
